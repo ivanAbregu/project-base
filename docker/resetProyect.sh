fab5c422acf57d4f541a5e6663dd94ba382c85e9
ivan <ivan@bytelion.com>
2020-05-04 23:25:55 -0300

    first commit

---
+++ docker/resetProyect.sh
@@ -0,0 +1,12 @@
+# Borra las migraciones se borra la BD y se levanta nuevamente
+docker-compose down
+
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+
+find . -path "*/migrations/*.pyc" -delete
+
+docker container prune --force
+
+docker volume prune --force
+
+#docker-compose -f ./../docker-compose.yml up
